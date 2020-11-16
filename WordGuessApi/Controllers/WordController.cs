7265490f2cd0711d7f1f67a150d77c44cfe4eb51
Adam Rybak <arserwiskolo@hotmail.com>
2020-11-15 23:00:29 -0800

    first commit

---
+++ WordGuessApi/Controllers/WordController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WordGuess;
+using WordGuess.Models;
+using WordGuess.Storage;
+
+namespace WordGuessWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+        // Private data member 
+        private WordGuessSystem _library;
+
+        public WordController(WordGuessSystem library)
+        {
+            //var wordStorage = new wordStorageList();
+            //var playerStorage = new playerStorageList();
+            _library = library;//new WordGuessSystem(wordStorage, playerStorage);
+        }
+
+        // Return all books at the library
+        [HttpGet]
+        public List<Word> GetAllWords()
+        {
+            List<Word> results = _library.GetAllWords();
+            return results;
+        }
+
+        [HttpPost]
+        public string PopulateLetter(Word newWord) 
+        {
+            _library.AddNewWord(newWord);
+            return "Word added";
+        }
+
+    }
+}
---
+++ WordGuessApi/Word.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WordGuessApi
+{
+    public class Word
+    {
+        //public DateTime Date { get; set; }
+
+        public int Id { get; set; }
+
+        public string Category { get; set; }
+
+        public string Value { get; set; }
+    }
+}
